fab298acfe70d138947848c59fcf360b32980ebb
Antonio Cervone <ant.cervone@gmail.com>
2020-07-10 15:03:46 +0200

    eccodes: add new versions

--- var/spack/repos/builtin/packages/eccodes/package.py
+++ var/spack/repos/builtin/packages/eccodes/package.py
@@ -11,17 +11,23 @@ class Eccodes(CMakePackage):
     """ecCodes is a package developed by ECMWF for processing meteorological
     data in GRIB (1/2), BUFR (3/4) and GTS header formats."""
 
-    homepage = 'https://software.ecmwf.int/wiki/display/ECC/ecCodes+Home'
-    url = 'https://software.ecmwf.int/wiki/download/attachments/45757960/eccodes-2.2.0-Source.tar.gz?api=v2'
-    list_url = 'https://software.ecmwf.int/wiki/display/ECC/Releases'
+    homepage = 'https://confluence.ecmwf.int/display/ECC/ecCodes+Home'
+    url = 'https://confluence.ecmwf.int/download/attachments/45757960/eccodes-2.24.1-Source.tar.gz'
+    list_url = 'https://confluence.ecmwf.int/display/ECC/Releases'
 
     maintainers = ['skosukhin']
 
+    version('2.24.1', sha256='e838523d8c895fcc0b7dfa7ee1ac107de70ec7456e80e46a8af013196f429e1e')
+    version('2.24.0', sha256='307ae2b79f4a7d31789824bca1df1412f60cad68f49bfc49bbc1d4780f3e5b96')
     version('2.21.0', sha256='da0a0bf184bb436052e3eae582defafecdb7c08cdaab7216780476e49b509755')
     version('2.20.0', sha256='207a3d7966e75d85920569b55a19824673e8cd0b50db4c4dac2d3d52eacd7985')
     version('2.19.1', sha256='9964bed5058e873d514bd4920951122a95963128b12f55aa199d9afbafdd5d4b')
+    version('2.19.0', sha256='a1d080aed1b17a9d4e3aecccc5a328c057830cd4d54f451f5498b80b24c46404')
     version('2.18.0', sha256='d88943df0f246843a1a062796edbf709ef911de7269648eef864be259e9704e3')
+    version('2.17.0', sha256='762d6b71993b54f65369d508f88e4c99e27d2c639c57a5978c284c49133cc335')
     version('2.13.0', sha256='c5ce1183b5257929fc1f1c8496239e52650707cfab24f4e0e1f1a471135b8272')
+    version('2.12.5', sha256='c5ce1183b5257929fc1f1c8496239e52650707cfab24f4e0e1f1a471135b8272')
+    version('2.10.0', sha256='c5ce1183b5257929fc1f1c8496239e52650707cfab24f4e0e1f1a471135b8272')
     version('2.5.0', sha256='18ab44bc444168fd324d07f7dea94f89e056f5c5cd973e818c8783f952702e4e')
     version('2.2.0', sha256='1a4112196497b8421480e2a0a1164071221e467853486577c4f07627a702f4c3')
 
@@ -71,7 +77,7 @@ class Eccodes(CMakePackage):
     patch('openjpeg_jasper.patch', when='@:2.16')
 
     # CMAKE_INSTALL_RPATH must be a semicolon-separated list.
-    patch('cmake_install_rpath.patch', when='@:2.10')
+    patch('cmake_install_rpath.patch', when='@:2.9')
 
     @run_before('cmake')
     def check_fortran(self):
@@ -141,6 +147,8 @@ class Eccodes(CMakePackage):
         if '^python' in self.spec:
             args.append('-DPYTHON_EXECUTABLE:FILEPATH=' + python.path)
 
+        if '+fortran' in self.spec:
+            args.append('-DCMAKE_Fortran_FLAGS=-fallow-argument-mismatch')
         return args
 
     def check(self):
